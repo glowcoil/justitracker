9fb6385aa6f4164dbff2d1aa75e43945b48db503
Micah Johnston <micah@glowcoil.com>
2019-11-23 12:01:48 -0600

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+extern crate glfw;
 
+use glfw::{Action, Context, Key};
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw.create_window(800, 600, "justitracker", glfw::WindowMode::Windowed).unwrap();
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
 }
