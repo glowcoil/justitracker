36723507a92b2409595183b01a9b127e3682c28a
Micah Johnston <micah@glowcoil.com>
2019-11-23 12:37:34 -0600

    gouache

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate gl;
 extern crate glfw;
+extern crate gouache;
 extern crate portaudio;
 
 use glfw::{Action, Context, Key};
+use gouache::{renderers::GlRenderer, Cache, Color, Font, Frame, Mat2x2, Vec2};
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
     let (mut window, events) = glfw.create_window(800, 600, "justitracker", glfw::WindowMode::Windowed).unwrap();
 
     window.set_key_polling(true);
@@ -14,11 +18,11 @@ fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    unsafe {
-        gl::ClearColor(1.0, 0.0, 0.8, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
-    }
-    window.swap_buffers();
+    let mut cache = Cache::new();
+    let mut renderer = GlRenderer::new();
+
+    let font = Font::from_bytes(include_bytes!("../res/SourceSansPro-Regular.ttf")).unwrap();
+    let text = font.layout("justitracker", 14.0);
 
     audio().unwrap();
 
@@ -32,6 +36,14 @@ fn main() {
                 _ => {}
             }
         }
+
+        let mut frame = Frame::new(&mut cache, &mut renderer, 800.0, 600.0);
+
+        frame.clear(Color::rgba(0.1, 0.15, 0.2, 1.0));
+        frame.draw_text(&font, &text, Vec2::new(0.0, 0.0), Mat2x2::id(), Color::rgba(1.0, 1.0, 1.0, 1.0));
+        frame.finish();
+
+        window.swap_buffers();
     }
 }
 
